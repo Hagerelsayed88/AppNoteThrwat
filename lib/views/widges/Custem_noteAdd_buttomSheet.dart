7450ed1d067a397da12abfe399be799d3fe23600
Hagerelsayed88 <dodo1141988@gmail.com>
2025-09-24 14:43:41 +0300

    showModleButtomSheet

---
+++ lib/views/widges/Custem_noteAdd_buttomSheet.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/cupertino.dart';
+
+class addNoteButoomsheet extends StatelessWidget {
+  const addNoteButoomsheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
