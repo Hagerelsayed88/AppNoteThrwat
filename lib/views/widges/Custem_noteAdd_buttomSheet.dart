e1291d59989e8410394351293d5e37ee185af35a
Hagerelsayed88 <dodo1141988@gmail.com>
2025-10-18 18:04:59 +0300

    add field to buttomsheet

--- lib/views/widges/Custem_noteAdd_buttomSheet.dart
+++ lib/views/widges/Custem_noteAdd_buttomSheet.dart
@@ -1,11 +1,25 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'custem_textfield.dart';
 
 class addNoteButoomsheet extends StatelessWidget {
   const addNoteButoomsheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+      child: Column(
+        children: [
+         // SizedBox(height: 10,),
+          custemTextfield(hint: 'title',),
+          SizedBox(height: 20,),
+          custemTextfield(hint: 'content',maxline: 5,),
+        ],
+      ),
+    );a
   }
 }
+
--- lib/views/widges/custem_textfield.dart
+++ lib/views/widges/custem_textfield.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:notetharwat/views/constants.dart';
 
 class custemTextfield extends StatelessWidget {
-  const custemTextfield({super.key});
-
+  const custemTextfield({super.key, required this.hint, this.maxline=1});
+ final String hint;
+ final int maxline;
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: maxline,
      decoration: InputDecoration(
-       hintText: "title",
+       hintText: hint,
        hintStyle: TextStyle(color: khintTextColor),
        border: OutlineInputBorder(
          borderRadius: BorderRadius.circular(20),
