17c99750b9014ee8f00c8d1b4e3b60d6ef79221f
Hagerelsayed88 <dodo1141988@gmail.com>
2025-10-25 14:51:16 +0300

    change in appBar

--- lib/views/widges/custem_notes_view_appBar.dart
+++ lib/views/widges/custem_notes_view_appBar.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'custem_notes_view_searchIcon.dart';
-
 class CustemAppBar extends StatelessWidget {
-  const CustemAppBar({super.key});
-
+  const CustemAppBar({super.key, required this.text, required this.icon});
+ final String text;
+final IconButton icon;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text("Notes", style: TextStyle(fontSize: 25)),
+        Text(text, style: TextStyle(fontSize: 25)),
         Spacer(),
-        CustemSearchIcon(),
-      ],
+         Container(
+        height: 35,
+    width: 35,
+    decoration: BoxDecoration(
+    color: Colors.white.withAlpha((0.08 * 255).toInt()),
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    ),
+
+    child: Center(child: icon),
+    )
+
+    ],
     );
   }
 }
