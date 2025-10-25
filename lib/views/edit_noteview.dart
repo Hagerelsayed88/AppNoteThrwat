3fbef4ccea54156ecbc0b28e4b35cfb7fb7264d6
Hagerelsayed88 <dodo1141988@gmail.com>
2025-10-25 14:03:37 +0300

    creat edit Note view

---
+++ lib/views/edit_noteview.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class EditNoteview extends StatelessWidget {
+  const EditNoteview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
+
+
+
